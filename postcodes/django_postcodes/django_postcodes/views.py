aadb6aaf224b6d5acef5a224298a8e67a4ecd861
Benjamin Nava <arielnmz@gmail.com>
2022-01-16 19:42:41 -0600

    Receive a geolocations payload and process a result from the postcodes API

---
+++ postcodes/django_postcodes/django_postcodes/views.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+
+
+@csrf_exempt
+def reverse_geocode_postcodes_batch(request):
+    if request.method == "POST":
+        # Expect a JSON array in the POST body
+        geolocations = json.loads(request.body)
+
+        # Pass the geolocations object to the external API to get the result
+        result = requests.post("http://api.postcodes.io/postcodes", json=geolocations)
+
+        return JsonResponse({"ok": True, "geolocations": result.json()})
+
+    return HttpResponse("Bad request", code=400)
