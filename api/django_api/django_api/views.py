87366d87c0e527c96274c041c98d627b03bd18c8
Benjamin Nava <arielnmz@gmail.com>
2022-01-16 19:00:06 -0600

    Store csv to file and read back assuming encoding

--- api/django_api/django_api/views.py
+++ api/django_api/django_api/views.py
@@ -1,7 +1,5 @@
 import csv
-from codecs import iterdecode
 from pathlib import Path
-from tempfile import TemporaryFile
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -15,17 +13,27 @@ def request_postcodes_from_csv(request):
         form = CsvUploadForm(request.POST, request.FILES)
         if form.is_valid():
             csv_file = request.FILES["csv_file"]
-            csv_reader = csv.reader(iterdecode(csv_file, "utf-8"))
-            _ = 0
-            # Skip head
-            next(csv_reader)
-            for row in csv_reader:
-                lat, lon = row
-                print(lat, lon)
-                _ += 1
-                if _ > 9:
-                    break
 
-            return HttpResponse("Hello, world. You're at the polls index.")
+            # Store file in a file REQ: Receive file and store it
+            with Path("postcodes.csv").open("wb") as f:
+                for chunk in csv_file.chunks():
+                    f.write(chunk)
+
+            # Read back from the created file to process the CSV
+            with Path("postcodes.csv").open("r") as f:
+                csv_reader = csv.reader(f)
+                _ = 0
+                # Skip head
+                next(csv_reader)
+
+                for row in csv_reader:
+                    lat, lon = row
+                    print(lat, lon)
+
+                    _ += 1
+                    if _ > 9:
+                        break
+
+                return HttpResponse("Hello, world. You're at the polls index.")
 
     return HttpResponse("Bad request", code=400)
