39b388e49eaf4cd50f098b65bc3ea1fc3b62c9aa
Benjamin Nava <arielnmz@gmail.com>
2022-01-16 22:16:14 -0600

    Services integration

--- api/django_api/django_api/settings.py
+++ api/django_api/django_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-y9c@)@&z8km%gh8pq)j%h#148^-)571ta5czdt+hwha0#flkd(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
